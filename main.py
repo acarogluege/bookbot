d75a60fa2f6ef31376906c02a18acf67d63e885f
acarogluege <acarogluege@gmail.com>
2025-05-10 18:56:13 +0300

    Changed the variables

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from stats import get_word_count
-
+from stats import count_character
 def main():
-    print(f'{get_word_count("books/frankenstein.txt")} words found in the document')
-
+    words_found = f'{get_word_count("books/frankenstein.txt")} words found in the document'
+    print(words_found)
+    character_calculated = f'{count_character("books/frankenstein.txt")}'
+    print(character_calculated)
 main()
 
 
--- stats.py
+++ stats.py
@@ -5,5 +5,17 @@ def get_word_count(filepath):
     total_words = len(stringify_text)
     return total_words
 
-
+def count_character(filepath, ):
+    with open(filepath, 'r') as f:
+        text = f.read().lower()
+    stringify_text = text.split()
+    char_counts = {}
+    for word in stringify_text:
+        for char in word:
+            if char not in char_counts:
+                char_counts[char] = 1
+            else:
+                char_counts[char] += 1
+    return char_counts
+    
     
